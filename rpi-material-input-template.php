e961633d865f9eef24a2549288eb2667af279f53
Joachim Happel <joachim.happel@gmail.com>
2022-05-19 11:42:17 +0200

    add css

--- rpi-material-input-template.php
+++ rpi-material-input-template.php
@@ -153,17 +153,7 @@ class RpiMaterialInputTemplate
 	    $args = [
 		    "label" => __( "Materialvorlagen", "blocksy" ),
 		    "labels" => $labels,
-		    "description" => "",
-		    "public" => true,
-		    "publicly_queryable" => true,
-		    "show_ui" => true,
-		    "show_in_rest" => true,
-		    "rest_base" => "",
-		    "rest_controller_class" => "WP_REST_Posts_Controller",
-		    "has_archive" => false,
-		    "show_in_menu" => true,
-		    "show_in_nav_menus" => true,
-		    "delete_with_user" => false,
+
 		    "exclude_from_search" => false,
 		    "capability_type" => "post",
 		    "map_meta_cap" => true,
